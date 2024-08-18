183e7ae346c5038c41c121a11969ec5079a3a5a1
postcalm <infinityrestriction@gmail.com>
2024-08-18 20:49:35 +0700

    Add copy so lib

--- {{cookiecutter.out_dir}}/windows/runner/main.cpp
+++ {{cookiecutter.out_dir}}/windows/runner/main.cpp
@@ -1,9 +1,35 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #include "flutter_window.h"
 #include "utils.h"
+
+void useSoFile(const char* name) {
+    std::ifstream fin(name, std::ios::binary);
+    std::ofstream fout("data\\app.so", std::ios::binary);
+
+    bool bRet = true;
+
+    while(!fin.eof()) {
+        char szBuf;
+        fin.read(&szBuf, sizeof(char));
+
+        if(fin.eof()) break;
+
+        if (fout.bad()) {
+            bRet = false;
+            break;
+        }
+        fout.write(&szBuf, sizeof(char));
+    }
+
+    fout.close();
+    fin.close();
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command)
@@ -14,6 +40,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   {
     CreateAndAttachConsole();
   }
+
+  useSoFile("data\\{{ cookiecutter.project_name }}.so");
 
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
